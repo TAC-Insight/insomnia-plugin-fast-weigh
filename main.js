6671d39fa0445e8f39c8258774b6ff155fb0bd4b
Eric Wilhite <eswilhite@gmail.com>
2022-02-01 09:50:53 -0500

    isarray

--- main.js
+++ main.js
@@ -8,17 +8,49 @@ module.exports.requestActions = [
       const { request } = data;
       let body = JSON.parse(request.body.text);
       let telerikBody = {};
+      let variables = {};
       telerikBody.query = body.query;
-      telerikBody.variables = {};
+      telerikBody.variables = [];
 
       if (typeof body.variables !== "undefined") {
         // map over variables and create @params
         Object.keys(body.variables).forEach((key) => {
-          telerikBody.variables[key] = "@" + key;
+          const telerikParam = "@" + key;
+          telerikBody.variables[key] = telerikParam;
+
+          // push variable type to variables array
+          if (Array.isArray(body.variables[key])) {
+            // typeof array
+            variables.push({ telerikParam: "array" });
+          } else {
+            variables.push({ telerikParam: typeof body.variables[key] });
+          }
         });
       }
 
       telerikJsonBody = JSON.stringify(telerikBody, null, 2);
+
+      // strip quotes from array parameters and int/float parameters
+      variables.forEach((variable) => {
+        if (variable.telerikParam === "array") {
+          telerikJsonBody = telerikJsonBody.replace(
+            '"@' + variable.telerikParam + '"',
+            "[" + "@" + variable.telerikParam + "]"
+          );
+        } else if (
+          variable.telerikParam === "number" ||
+          variable.telerikParam === "bigint" ||
+          variable.telerikParam === "undefined" ||
+          variable.telerikParam === "boolean"
+        ) {
+          telerikJsonBody = telerikJsonBody.replace(
+            '"@' + variable.telerikParam + '"',
+            "@" + variable.telerikParam
+          );
+        }
+      });
+
+      // write text to clipboard
       navigator.clipboard.writeText(telerikJsonBody);
 
       const html = `
